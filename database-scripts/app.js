4bc5c62382cbda5ab342d71b6e78c00a26607bd7
iconstant <160066687+iconstant@users.noreply.github.com>
2024-02-29 22:09:55 -0500

    Geting rid of example files
    added 3 other urls

--- database-scripts/app.js
+++ database-scripts/app.js
@@ -8,19 +8,23 @@ const app = express();
 
 // webpage display
 app.get('/api', (req, res) => { // localhost:3000/api
-    res.json(`HTTP GET request recieved`)
+    res.json(`HTTP GET request recieved`) // print msg on page ig
 })
 
-// app.use(express.static(path.join(__dirname + '/public0'))); // http://localhost:3000/
-app.use('/website', express.static(path.join(__dirname, 'public0'))); // run on subdir of root (http://localhost:3000/website/)
+app.use('/gamezcentral', express.static(path.join(__dirname, 'public0/Home.html'))); // home page (http://localhost:80/gamezcentral)
+app.use('/gamezcentral/chess', express.static(path.join(__dirname, 'public0/Chess.html'))); // home page (http://localhost:80/gamezcentral/chess)
+app.use('/gamezcentral/risk', express.static(path.join(__dirname, 'public0/Risk.html'))); // home page (http://localhost:80/gamezcentral/risk)
+app.use('/gamezcentral/tictactoe', express.static(path.join(__dirname, 'public0/Tic-Tac-Toe.html'))); // home page (http://localhost:80/gamezcentral/tictactoe)
 
-app.use('/', express.static(path.join(__dirname + '/react-app/build'))); // ex of react application hosting
 
 app.use((req, res) => {
     res.status(404);
     res.send(`<h1>Error 404: Resource not found</h1>`);
 })
 
-app.listen(80, () => {
+app.listen(80, () => { // http://localhost:80/
     console.log("App listening on port 80");
 })
+
+// app.use('/<name>', express.static(path.join(__dirname, '<html>'))); // run on subdir of root (http://localhost:80/<name>/)
+//app.use('/', express.static(path.join(__dirname + '/react-app/build'))); // ex of react application hosting
