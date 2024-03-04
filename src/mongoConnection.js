5447a8ed86e378f43bf6e23c2e93bc3e29581691
jrohrbaugh0812 <160036221+jrohrbaugh0812@users.noreply.github.com>
2024-03-04 09:30:41 -0500

    Removed "{ useNewUrlParser: true, useUnifiedTopology: true }"

--- src/mongoConnection.js
+++ src/mongoConnection.js
@@ -2,7 +2,7 @@
 const { MongoClient } = require('mongodb');
 
 const uri = 'mongodb://127.0.0.1:27017/webappproject';
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri);
 
 async function connectToDatabase() {
   try {
