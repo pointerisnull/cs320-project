0abf0f002964410d7120e4f528f1165ce16eac87
jrohrbaugh0812 <jrohrbaugh3@ycp.edu>
2024-03-17 23:05:03 -0400

    Users can now sign in and remain in a persistent login state. Also, there were many other changes to various files.

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const querystring = require('querystring');
 const { createUser } = require('./database-scripts/User/UserInsert');
-const { handleLeaderboardRequest } = require('./handlers/leaderBoardHandler'); // Import the function
+const { handleLeaderboardRequest } = require('./handlers/leaderBoardHandler');
 const { handleUserAuthentication } = require('./handlers/authenticateUserHandler');
+const { secretKey, generateToken, verifyToken } = require('./handlers/JWTTokenHandler');
+const { handleUserDataRequest } = require('./handlers/userDataHandler');
 
 const server = http.createServer((req, res) => {
     let filePath = path.join(__dirname, req.url);
@@ -30,12 +32,13 @@ const server = http.createServer((req, res) => {
                 const formData = querystring.parse(data);
                 console.log('Parsed form data:', formData);
 
-                //'formData' contains user input
+                // Now 'formData' will contain the user input
                 const email = formData.email;
                 const username = formData.username;
                 const password = formData.password;
                 const avatar = formData.avatar;
-                
+
+                // Move MongoDB operations here
                 await createUser(username, password, email, avatar);
 
                 // Respond to the client
@@ -43,46 +46,75 @@ const server = http.createServer((req, res) => {
             } catch (error) {
                 console.error('Error processing form data:', error);
                 res.end('Internal Server Error');
-            } finally {
-                res.end();
             }
         });
+        return;
     }
 
     // Handle POST request for login
     if (req.method === 'POST' && req.url === '/html/login.html') {
         let data = '';
+        // Accumulate request data
         req.on('data', chunk => {
             data += chunk;
         });
+        // Handle request end
         req.on('end', async () => {
             try {
+                // Parse incoming data
                 const { username, password } = JSON.parse(data);
+                // Check user authentication
                 const isAuthenticated = await handleUserAuthentication(username, password);
+
                 if(isAuthenticated) {
                     console.log('User authenticated successfully');
+                    // Generate token for the user
+                    const token = generateToken({ username });
+                    // Set response status code and send token
                     res.statusCode = 200;
-                    res.end();
-                    return;
+                    res.end(JSON.stringify({ token }));
                 }
                 else {
+                    // If user authentication fails, set response status code to 401
                     console.log('User authentication failed');
                     res.statusCode = 401;
                     res.end();
-                    return;
                 }
             } catch (error) {
+                // Handle errors during login request processing
                 console.error('Error processing login request:', error);
                 res.statusCode = 500;
                 res.end('Internal Server Error');
-                return;
             }
         });
+        return;
     }
     
     // Handles the leaderboard get request
     if (req.method === 'GET' && req.url === '/leaderboard-data') {
         handleLeaderboardRequest(req, res);
+        return; // End the request here to prevent further processing
+    }
+
+    // Handles the user data get request
+    if (req.method === 'GET' && req.url === '/user-data') {
+        // Extract token from request headers
+        const token = req.headers.authorization.split(' ')[1];
+        // Verify the token
+        const decodedToken = verifyToken(token);
+
+        // Check if token is invalid or expired
+        if (!decodedToken) {
+            // Token is invalid or expired
+            res.statusCode = 401; // Unauthorized
+            res.end('Unauthorized');
+            return;
+        }
+
+        // Extract username from decoded token
+        const username = decodedToken.username;
+        // Process user data request
+        handleUserDataRequest(req, res, username);
         return; // End the request here to prevent further processing
     }
     
@@ -123,8 +155,7 @@ const server = http.createServer((req, res) => {
             }
 
             // Set the appropriate content type
-            res.setHeader('Content-Type', contentType);
-            res.writeHead(200);
+            res.writeHead(200, { 'Content-Type': contentType });
             res.end(data);
         }
     });
