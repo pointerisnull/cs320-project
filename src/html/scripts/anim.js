59fe0530c8a03975a548cb3f3819faf8c43b658d
iconstant <160066687+iconstant@users.noreply.github.com>
2024-03-20 08:44:24 -0400

    chess IM
    start of animation file for all SVG's/Image Maps

--- src/html/scripts/anim.js
+++ src/html/scripts/anim.js
@@ -1,3 +1,12 @@
+// Chess
+function piecesDisplay(altInfo) {
+  alert("clicked on piece " + altInfo);
+}
+function boardDisplay(altInfo) {
+  alert("clicked on square " + altInfo);
+}
+
+// Tic Tac Toe
 function anim() { // https://www.w3schools.com/jsref/met_win_settimeout.asp
     l = document.getElementById("win");
     o0 = document.getElementById("0x0");
@@ -24,3 +33,5 @@ function anim() { // https://www.w3schools.com/jsref/met_win_settimeout.asp
     setTimeout(function(){ o1.style.fill="#000000"}, 6000);
     setTimeout(function(){ l.style.stroke="#000000"}, 7000);
   }
+
+// Risk
