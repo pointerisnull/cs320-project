ee28e2e4f9ef294c0c0b19a37fa570b7b188c07d
iconstant <160066687+iconstant@users.noreply.github.com>
2024-03-26 17:10:48 -0400

    Chess page update
    also updated chess image map that can be a reference in the future

--- src/html/Media/gamez/riskMaps/riskSVGMap/scriptRiskPage.js
+++ src/html/Media/gamez/riskMaps/riskSVGMap/scriptRiskPage.js
@@ -1,1 +1,115 @@
 const continents = document.querySelectorAll('.North_America, .South_America, .Europe, .Africa, .Asia, .Australia');
+
+let zoomTimeout; // Variable to hold zoom timeout
+let changeColorTimeout; // Variable to hold color change timeout
+let colorChangeInProgress = false; // Flag to indicate if color change animation is in progress
+
+// Function to handle mouse enter event
+function handleMouseEnter() {
+    clearTimeout(changeColorTimeout); // Clear color change timeout if it's active
+    clearTimeout(zoomTimeout); // Clear zoom timeout if it's active
+    colorChangeInProgress = false; // Reset color change animation state
+    zoomInToNorthAmerica(); // Start zoom animation
+}
+
+// Function to handle mouse leave event
+function handleMouseLeave() {
+    clearTimeout(zoomTimeout); // Clear zoom timeout if it's active
+    clearTimeout(changeColorTimeout); // Clear color change timeout if it's active
+    colorChangeInProgress = false; // Set flag to stop color change animation
+
+    continents.forEach(continent => {
+        // Reset each continent to its original color
+        switch (continent.getAttribute('class')) {
+            case "South_America":
+                continent.style.stroke = "#4d9900";
+                continent.style.fill = "#d8e9b6";
+                break;
+            case "Europe":
+                continent.style.stroke = "#cc0066";
+                continent.style.fill = "#f0d6e1";
+                break;
+            case "Africa":
+                continent.style.stroke = "#ff6633";
+                continent.style.fill = "#fad8be";
+                break;
+            case "Asia":
+                continent.style.stroke = "#99cc00";
+                continent.style.fill = "#e2f0cb";
+                break;
+            case "Australia":
+                continent.style.stroke = "#0077cc";
+                continent.style.fill = "#85c1ff";
+                break;
+            default:
+                break;
+        }
+    });
+    worldMap.setAttribute('viewBox', '0 0 800 533'); // Reset viewBox
+}
+
+// Function to change the color of a continent
+function changeContinentColor() {
+    // Define the delay between each continent color change
+    const delay = 500; // Adjust this value as needed
+    let variableSpeed = 1
+
+    // Counter variable to track the delay for each continent
+    let delayCounter = 0;
+
+    colorChangeInProgress = true;
+
+    // Loop through each continent
+    continents.forEach(continent => {
+        // Variably increase the delay counter for each continent
+        variableSpeed += 0.05;
+        delayCounter += delay / variableSpeed;
+
+        // Set a timeout to change the continent color after a delay
+        changeColorTimeout = setTimeout(() => {
+            if(!colorChangeInProgress) return;
+            continent.style.stroke = '#0066cc';
+            continent.style.fill = '#cce5ff'; // Set new color
+        }, delayCounter); // Set the delay for this continent
+    });
+}
+
+const mapContainer = document.getElementById('mapContainer');
+const worldMap = document.getElementById('worldMap');
+
+// Function to zoom in to the North American continent
+function zoomInToNorthAmerica() {
+    let viewBoxWidth = 800;
+    let viewBoxHeight = 533;
+
+    function zoomIn() {
+        if (viewBoxWidth >= 400) {
+            viewBoxWidth -= 4;
+            viewBoxHeight -= 1;
+            worldMap.setAttribute('viewBox', `0 0 ${viewBoxWidth} ${viewBoxHeight}`);
+            mapContainer.style.transform = 'scale(1.05)';
+            zoomTimeout = setTimeout(zoomIn, 16); // Use requestAnimationFrame equivalent timeout
+        } else {
+            setTimeout(zoomOut, 2000); // Call zoomOut after 2 seconds
+        }
+    }
+
+    function zoomOut() {
+        if (viewBoxWidth < 800 || viewBoxHeight < 533) {
+            if (viewBoxWidth < 800) {
+                viewBoxWidth += 8;
+            }
+            if (viewBoxHeight < 533) {
+                viewBoxHeight += 4;
+            }
+            worldMap.setAttribute('viewBox', `0 0 ${viewBoxWidth} ${viewBoxHeight}`);
+            mapContainer.style.transform = 'scale(1.05)';
+            zoomTimeout = setTimeout(zoomOut, 16); // Use requestAnimationFrame equivalent timeout
+        } else {
+            changeContinentColor();
+        }
+    }
+
+    // Start zooming in
+    zoomIn();
+}
--- src/html/scripts/audio.js
+++ src/html/scripts/audio.js
@@ -3,45 +3,30 @@ var toggleAudio = document.getElementById('toggleBtn');
 var volumeSlider = document.getElementById('volumeSlider');
 var icon = document.getElementById('icon');
 
+// Initialize audio context
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var source = audioCtx.createMediaElementSource(audio);
+var gainNode = audioCtx.createGain();
 
-var audioCtx;
-var source;
-var gainNode;
+// Connect the audio source to the gain node
+source.connect(gainNode);
+gainNode.connect(audioCtx.destination);
 
 // Event listener for the toggle button
 toggleAudio.addEventListener('click', function () {
-
-    // Initialize audio context and gainNode outside of event listeners if there isn't already a source.
-    if(!source) {
-        audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        source = audioCtx.createMediaElementSource(audio);
-        gainNode = audioCtx.createGain();
-    }
-
-    // Connect the audio source to the gain node
-    source.connect(gainNode);
-    gainNode.connect(audioCtx.destination);
-
     if (audio.paused) {
         audio.play();
-        // Change icon when audio is playing
-        icon.classList.remove('fa-volume-xmark');
-        icon.classList.add('fa-volume-high');
+        // change icon img NOT DONE
+        icon.classList.remove('fa-solid fa-volume-xmark');
+        icon.classList.add('fa-solid fa-volume-high');
     } else {
         audio.pause();
-        // Change icon when audio is paused
-        icon.classList.remove('fa-volume-high');
-        icon.classList.add('fa-volume-xmark');
+        icon.classList.remove('fa-solid fa-volume-high');
+        icon.classList.add('fa-solid fa-volume-xmark');
     }
 });
 
-// Event listener for the volume slider
 volumeSlider.addEventListener('input', function () {
     var volume = parseFloat(volumeSlider.value);
-    console.log("Volume:", volume); // Debugging statement
     gainNode.gain.value = volume;
-    console.log("Gain Node Gain:", gainNode.gain.value); // Debugging statement
 });
-
-// Debugging statement to check if audio element is loaded
-console.log("Audio Element:", audio);
