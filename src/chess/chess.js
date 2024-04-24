1fe508fc6537abcc3e035808fdc942a47eb890a0
Bren <92599085+pointerisnull@users.noreply.github.com>
2024-04-24 01:30:00 -0400

    logic update

--- src/chess/chess.js
+++ src/chess/chess.js
@@ -1,5 +1,6 @@
 let captured_pieces = [];
 let turn = true;
+
 const board_squares = document.getElementsByClassName("square");
 const pieces = document.getElementsByClassName("piece");
 const piece_images = document.getElementsByTagName("img");
@@ -23,51 +24,108 @@ function check_coords(col, row) {
     if (board_squares[i].id == coords)
       square = board_squares[i];
   }
-  console.log(square);
 
   return is_occupied(square);
 }
 
-function is_check() {
-  /*TODO*/
+function hide_legal_squares() {
+  for (let i = 0; i < board_squares.length; i++) {
+    if (board_squares[i].classList.contains("active"))
+      board_squares[i].classList.remove("active");
+  }
+}
+
+function show_legal_squares(piece) {
+  for (let i = 0; i < board_squares.length; i++) {
+    if (is_legal_move(piece, board_squares[i]))
+      board_squares[i].classList.add("active");
+  }
+}
+
+function simulate_move() {
+
+}
+
+function en_passant(fcol, frow, tocol, torow, occupation, color) {
+  if ((frow-torow == 1) &&  (tocol+1 == fcol || tocol-1 == fcol) && occupation == "empty" && color == "black")
+    return true;
+  else if ((torow-frow == 1) && (tocol+1 == fcol || tocol-1 == fcol) && occupation == "empty" && color == "white")
+    return true;
   return false;
+}
+
+function is_check(color) {
+  let king;
+  let check = false;
+  for (let i = 0; i < pieces.length; i++)
+    if (pieces[i].classList.contains("king") && pieces[i].getAttribute("color") == color)
+      king = pieces[i];
+  
+  const king_square = board_squares[king.square_id];
+  
+  for (let i = 0; i < pieces.length; i++) {
+    if (is_legal_move(pieces[i], king_square))
+      check = true;
+  }
+  if (check && !king_square.classList.contains("check"))
+    king_square.classList.add("check");
+  else if (!check)
+    king_square.classList.remove("check");
+  return check;
 }
 
 function pawn_promote(pawn) {
 /*TODO*/
+  pawn.classList.add("queen");
+  pawn.classList.remove("pawn");
+  pawn.children[0].innerHTML = "<img src = \"res/queen_black.svg\">"; 
+
 }
 /*piece legality*/
 function pawn_legality(pawn, fcol, frow, tocol, torow, occupation, color) {
+  let move = false;
+  console.log(pawn.children);
   /*first move, pawn can move foward 2 spaces (optional)*/ 
   if (pawn.first_move) {
     pawn.fist_move = false;
     /*ONLY returns if moving two spaces foward*/
     if (color == "white") {
-      if ((torow - frow == 1 || torow - frow == 2) && tocol == fcol && occupation == "empty")
-        return true;
+      if ((torow - frow == 1 || torow - frow == 2) && tocol == fcol && occupation == "empty") {
+        pawn.en_passant = true;
+        move = true;
+      }
     } else if (color == "black") {
       if ((frow-torow == 1 || frow - torow == 2) &&  tocol == fcol && occupation == "empty") {
-        return true;
+        pawn.en_passant = true;
+        move = true;
       }
     }
+  } else {
+    pawn.en_passant = false;
   }
   /*normal case*/
   if (color == "white") {
     if ((torow-frow == 1) && tocol == fcol && occupation == "empty")
-      return true;
+      move = true;
   } else if(color == "black") {
     if ((frow-torow == 1) &&  tocol == fcol && occupation == "empty")
-      return true;
+      move = true;
   }
   /*attacking*/
   if (color == "white") {
     if ((torow-frow == 1) && (tocol+1 == fcol || tocol-1 == fcol) && occupation != "empty" && occupation != color)
-      return true;
-  }else if (color == "black") {
+      move = true;
+    else if (en_passant(fcol, frow, tocol, torow, occupation, color))
+      move = true;
+   
+  } else if (color == "black") {
     if ((frow-torow == 1) &&  (tocol+1 == fcol || tocol-1 == fcol) && occupation != "empty" && occupation != color)
-      return true;
-  }
-  return false;
+      move = true;
+    else if (en_passant(fcol, frow, tocol, torow, occupation, color))
+      move = true;
+  } 
+  
+  return move;
 }
 
 function rook_legality(piece, fcol, frow, tocol, torow, occupation, color) {
@@ -82,7 +140,6 @@ function rook_legality(piece, fcol, frow, tocol, torow, occupation, color) {
           if(check_coords(colCheck, frow) != "empty") return false;
         } 
       }
-      console.log("here");
       if(occupation != "empty" && occupation == color) return false;
       return true;
     }
@@ -98,7 +155,6 @@ function rook_legality(piece, fcol, frow, tocol, torow, occupation, color) {
         }
       }
 
-      console.log("here");
       if(occupation != "empty" && occupation == color) return false;
       return true;
     }
@@ -107,48 +163,76 @@ function rook_legality(piece, fcol, frow, tocol, torow, occupation, color) {
 
 
 function bishop_legality(piece, fcol, frow, tocol, torow, occupation, color) {
-//north east
+    if (color == occupation) return false;
+    //south east
     if(tocol > fcol && torow < frow) {
       var cdist = tocol - fcol;
       var rdist = frow - torow;
-      if(cdist == rdist) {
-        if(color == occupation) return false;
-
-      } else return false;
-
+      if(cdist != rdist) {
+        return false;
+      } 
+      let rowCheck = torow+1;
+      let colCheck = tocol-1;
+      while (rowCheck < frow) {
+        if (check_coords(colCheck, rowCheck) != "empty")
+          return false;
+        rowCheck++;
+        colCheck--;
+      }
       return true;
     }
-    //north west
+    //south west
     if(fcol > tocol && torow < frow) {
       var cdist = fcol - tocol;
       var rdist = frow - torow;
-      if(cdist == rdist) {
-        if(color == occupation) return false;
-
-      } else return false;
+      if(cdist != rdist) {
+        return false;
+      }
+      let rowCheck = torow+1;
+      let colCheck = tocol+1;
+      while (rowCheck < frow) {
+        if (check_coords(colCheck, rowCheck) != "empty")
+          return false;
+        rowCheck++;
+        colCheck++;
+      }
+      return true;
       return true;
     }   
-    //south east
+    //north east
     if(tocol > fcol && torow > frow) {
       var cdist = tocol - fcol;
       var rdist = torow - frow;
-      if(cdist == rdist) {
-        if(color == occupation) return false;
-
-      } else return false;
+      if(cdist != rdist) {
+        return false;
+      }
+      let rowCheck = torow-1;
+      let colCheck = tocol-1;
+      while (rowCheck > frow) {
+        if (check_coords(colCheck, rowCheck) != "empty")
+          return false;
+        rowCheck--;
+        colCheck--;
+      }
  
-      return true;
+      return true; 
     }
 
-    //south west
+    //north west
     if(fcol > tocol && torow > frow) {
       var cdist = fcol - tocol;
       var rdist = torow - frow;
-      if(cdist == rdist) {
-        if(color == occupation) return false;
-
-      } else return false;
-     
+      if(cdist != rdist) {
+        return false;
+      } 
+      let rowCheck = torow-1;
+      let colCheck = tocol+1;
+      while (rowCheck > frow) {
+        if (check_coords(colCheck, rowCheck) != "empty")
+          return false;
+        rowCheck--;
+        colCheck++;
+      }
       return true;
     }   
 
@@ -170,23 +254,25 @@ function knight_legality(piece, fcol, frow, tocol, torow, occupation, color) {
       //south west
       if((torow - frow) == 2 && (fcol - tocol) == 1) return true;
       if((torow - frow) == 1 && (fcol - tocol) == 2) return true;
+
       }
   }
   return false;
 }
 
 function king_legality(piece, fcol, frow, tocol, torow, occupation, color) {
+  let move = false;
   if(color != occupation) {
   //vertical
     if(tocol == fcol) 
-      if(Math.abs(torow-frow) == 1) return true;
+      if(Math.abs(torow-frow) == 1) move = true;
       //horizontal
     if(torow == frow) 
-      if(Math.abs(tocol-fcol) == 1) return true;
+      if(Math.abs(tocol-fcol) == 1) move = true;
     //diagonal
-    if(Math.abs(tocol-fcol)==1 && Math.abs(frow-torow)==1) return true;
+    if(Math.abs(tocol-fcol)==1 && Math.abs(frow-torow)==1) move = true;
   }
-  return false;
+  return move;
 }
 
 function is_legal_move(piece, destination) {
@@ -198,8 +284,8 @@ function is_legal_move(piece, destination) {
 
   const current_color = piece.getAttribute("color");
   const occupation = is_occupied(destination);
-
-  if (piece.classList.contains("pawn"))
+  
+  if (piece.classList.contains("pawn")) 
     return pawn_legality(piece, from_col, from_row, to_col, to_row, occupation, current_color);
   else if (piece.classList.contains("rook"))
     return rook_legality(piece, from_col, from_row, to_col, to_row, occupation, current_color);
@@ -230,9 +316,11 @@ function drag(ev) {
   if ((turn && color == "white") || (!turn && color == "black")) {
     ev.dataTransfer.setData("text", piece.id);
   }
+  show_legal_squares(piece);
 }
 
 function drop(ev) {
+  hide_legal_squares();
   ev.preventDefault();
   let data = ev.dataTransfer.getData("text");
   if (!data) return;
@@ -244,36 +332,41 @@ function drop(ev) {
   if (!is_legal_move(piece, destination))
     return;
 
-  if (is_occupied(destination) == "empty") {
-    destination.appendChild(piece);
-    turn = !turn;
-  } else {
+  if (is_occupied(destination) != "empty") {
     /*capture enemy pieces and prevent self-capture*/
     if (turn == true && color == "white" && is_occupied(destination) == "black") {
       /*destroy all children then append piece*/
       while(destination.firstChild) {
-        /*console.log(destination.firstChild);
-        let child = destination.firstChild.getAttribute("class");
-        captured_pieces.push(child);*/
         destination.removeChild(destination.firstChild);
       }
-      destination.appendChild(piece);
-      turn = !turn;
     } else if (turn == false && color == "black" && is_occupied(destination) == "white") {
       /*destroy all children then append piece*/
       while(destination.firstChild) {
-        /*console.log(destination);
-        let child = destination.firstChild.getAttribute("class");
-        captured_pieces.push(child);*/
         destination.removeChild(destination.firstChild);
       } 
-      destination.appendChild(piece);
-      turn = !turn;
     }
   }
+  
+  /*change turns*/
+  destination.appendChild(piece);
+  if (piece.classList.contains("pawn")) {
+    if (color == "white" && destination_id[1] == '8')
+      pawn_promote(piece);
+    else if (color == "black" && destination_id[1] == '1')
+      pawn_promote(piece);
+    console.log(destination);
+  } else if (piece.classList.contains("king")) {
+    for (let i = 0; i < board_squares.length; i++)
+      if (board_squares[i].classList.contains("check") && is_occupied(board_squares[i]) == "empty")
+      board_squares[i].classList.remove("check");
+  }
+  turn = !turn;
+  document.getElementById("turn").innerHTML = turn ? "White's Turn" : "Black's Turn";
   /*update the piece's coordinates*/
   piece.first_move = false;
   piece.square_id = destination_id;
+  is_check("white");
+  is_check("black");
 }
 /*setup*/
 function init_board() {
@@ -285,7 +378,7 @@ function init_board() {
     let current_square = board_squares[i];
     current_square.id = col+row;
   }
-}
+} 
 
 function init_pieces() {
   for (let i = 0; i < pieces.length; i++) {
@@ -294,6 +387,7 @@ function init_pieces() {
     pieces[i].square_id = pieces[i].parentElement.id;
     pieces[i].id = pieces[i].className.split(" ")[1] + pieces[i].parentElement.id;
     pieces[i].first_move = true;
+    if (pieces[i].classList.contains("pawn")) pieces[i].en_passant = false;
   }
   for (let i = 0; i < piece_images.length; i++) {
     piece_images[i].setAttribute("draggable", false);
